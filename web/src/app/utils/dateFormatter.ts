94ba7304b62cf757c5d520608eed889b53535611
Angel <angelrivas0day@gmail.com>
2020-08-01 18:32:30 -0500

    Before ng mat 10

--- web/src/app/utils/dateFormatter.ts
+++ web/src/app/utils/dateFormatter.ts
@@ -1,4 +1,6 @@
 import { MatDateFormats, NativeDateAdapter } from '@angular/material/core';
+import { Injectable } from "@angular/core";
+@Injectable()
 export class AppDateAdapter extends NativeDateAdapter {
   format(date: Date, displayFormat: Object): string {
     if (displayFormat === 'input') {
